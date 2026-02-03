d0f985551774f6e89d96132a1779bd324d32204d
pashechkar-max <polinausakova921@gmail.com>
2026-02-03 10:39:54 +0700

    practic_qwe

--- js/main.js
+++ js/main.js
@@ -14,19 +14,28 @@ let app = new Vue({
             {
                 variantId: 2234,
                 variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
         sizes: ['S', 'M', 'L'],
         cart: 0,
-        methods: {
-            addToCart() {
-                this.cart += 1
-                }
-            }
-    }
+
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeToCart() {
+            this.cart -= 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+    },
 })
 
